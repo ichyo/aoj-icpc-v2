d6f8613b35317db2d5e463778e6be5f50b8dae86
ichyo <ichyo00@gmail.com>
2019-06-28 00:05:36 +0900

    Add problems.json

--- api/src/bin/initialize_db.rs
+++ api/src/bin/initialize_db.rs
@@ -1,13 +1,30 @@
 use aoj_icpc::db;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Problem {
+    title: String,
+    source: String,
+    point: i32,
+    url: String,
+}
 
 fn main() {
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL is required");
     let conn = db::establish_connection(&database_url);
-    let problems = vec![db::NewProblem {
-        title: "abc",
-        source: "aaa",
-        point: 120,
-        url: "https://doc.rust-lang.org/cargo/reference/manifest.html#the-project-layout",
-    }];
+    let problems: Vec<Problem> =
+        serde_json::from_str(include_str!("../../resources/problems.json")).unwrap();
+    let problems = problems
+        .iter()
+        .map(|p| db::NewProblem {
+            title: &p.title,
+            source: &p.source,
+            point: p.point,
+            url: &p.url,
+        })
+        .collect::<Vec<_>>();
+
     db::replace_problems(&conn, &problems);
+
+    println!("{} initialized", database_url);
 }
